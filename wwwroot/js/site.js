c2e2c4e74195ef61310306da58cace32d09097a7
Паша Алешкевич <aleskevic79@gmail.com>
2022-12-12 19:56:33 +0300

    Added a admin page spinner for page loading.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/*Preloader*/
+(function ($) {
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloader").delay(200).fadeOut("slow");
+    });
+})(jQuery);
